4ce962c2db794a34e9de17e51db897bf1bd6c498
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-10 15:26:03 -0500

    stubbed in gallery router and required in server.js

---
+++ server/routes/gallery.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// POST ROUTE
+
+// GET ROUTE
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const gallery = require('./routes/gallery.router');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /** -------- Serve static files --------- **/
 app.use(express.static('build'));
+
+/** -------- ROUTES --------- **/
+app.use('/api/gallery', gallery);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
