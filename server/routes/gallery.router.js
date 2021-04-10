b7dc104c39c716c7f40ebae3db6f2ce17c737893
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-10 16:03:09 -0500

    added POST route

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-// POST ROUTE
+// POST ROUTE - adds new gallery item s3 path to DB
+router.post('/', (req, res) => {
+  const imagePath = req.body.imagePath;
+
+  const queryText = `INSERT INTO "gallery" (image_path)
+  VALUES ($1);`;
+
+  pool
+    .query(queryText, [imagePath])
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('could not add the image path!', err);
+      res.sendStatus(500);
+    });
+});
 
 // GET ROUTE - retrieves all gallery items from DB
 router.get('/all', (req, res) => {
