7ff443bf594216e41dc0c58f6d43591480dce912
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-10 15:47:15 -0500

    added GET route to retrieve all gallery items

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,6 +4,19 @@ const pool = require('../modules/pool');
 
 // POST ROUTE
 
-// GET ROUTE
+// GET ROUTE - retrieves all gallery items from DB
+router.get('/all', (req, res) => {
+  const queryText = 'SELECT * FROM "gallery";';
+
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log('could not get gallery items!', err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
