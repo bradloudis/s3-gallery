f5bbb6358d2ce1d36b0b8de248fc9e509a46ff39
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-11 21:20:23 -0500

    initial server setup for s3 dropzone

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
+
+// s3 uploader var
+const UploaderS3Router = require('react-dropzone-s3-uploader/s3router');
 
 const gallery = require('./routes/gallery.router');
 
@@ -14,6 +18,17 @@ app.use(express.static('build'));
 
 /** -------- ROUTES --------- **/
 app.use('/api/gallery', gallery);
+
+// AWS S3
+app.use(
+  '/s3',
+  UploaderS3Router({
+    bucket: 'kontomo-test', // name of s3 bucket
+    region: 'us-east-2', // name of AWS Region
+    headers: { 'Access-Control-Allow-Origin': '*' }, // optional
+    ACL: 'public-read', // set to public-read
+  })
+);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
